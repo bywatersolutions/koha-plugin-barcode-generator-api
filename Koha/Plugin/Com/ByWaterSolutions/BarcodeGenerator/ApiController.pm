dcafb007a3329d3eedf433b06c241501b7e37a72
Kyle Hall <kyle@bywatersolutions.com>
2023-07-24 13:45:18 -0400

    Initial commit

---
+++ Koha/Plugin/Com/ByWaterSolutions/BarcodeGenerator/ApiController.pm
@@ -0,0 +1,75 @@
+package Koha::Plugin::Com::ByWaterSolutions::BarcodeGenerator::ApiController;
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This program comes with ABSOLUTELY NO WARRANTY;
+
+use Modern::Perl;
+
+use C4::Context;
+use C4::Auth qw(checkpw);
+
+use Mojo::Base 'Mojolicious::Controller';
+use Mojo::JSON qw(decode_json);
+use Encode qw(encode_utf8);
+use Template;
+
+use Try::Tiny;
+
+=head1 Koha::Plugin::Com::ByWaterSolutions::CoverFlow::Controller
+
+A class implementing the controller code for CoverFlow requests
+
+=head2 Class methods
+
+=head3 barcode
+
+Returns a generated PNG image of a barcode
+
+=cut
+
+sub generate {
+    my $c = shift->openapi->valid_input or return;
+
+    my $key     = $c->validation->param('key');
+    my $notext  = $c->validation->param('notext') ? 1 : 0;
+    my $type    = $c->validation->param('type') || 'Code39';
+    my $barcode = $c->validation->param('barcode');
+
+    my $BarcodeGeneratorPluginApiKey =
+      C4::Context->preference('BarcodeGeneratorPluginApiKey');
+
+    return try {
+        if (   $BarcodeGeneratorPluginApiKey
+            && $key ne $BarcodeGeneratorPluginApiKey )
+        {
+            return $c->render( status => 401 );
+        }
+
+        my $image =
+          GD::Barcode->new( $type, $barcode )->plot( NoText => $notext )->png();
+
+        print header('image/png');
+        print $image;
+        exit 0;
+    }
+    catch {
+        return $c->render(
+            status  => 500,
+            openapi => { error => "Unhandled exception ($_)" }
+        );
+    };
+}
+
+1;
---
+++ README.md
@@ -0,0 +1,28 @@
+# Barcode Generator API plugin for Koha
+
+This plugin adds a public barcode image generator API to Koha that outputs PNG files in the specified barcode format
+
+## Installation
+
+Just install this plugin and restart plack!
+
+## Configuration
+
+To require an API key, create a new local use system preference named `BarcodeGeneratorPluginApiKey` and set the API key you wish to use in there.
+Once this is set, any calls that do not have a URL parameter of `key` with this value will fail.
+
+## Usage
+
+This plugin exposes one API :
+
+### API path
+GET /api/v1/contrib/barcodegenerator/barcode?key=123456789&barcode=9876543241
+
+### Parameters
+
+#### user
+
+`barcode`: The barcode value to turn into an image
+`notext`: options, if true barcode image will not contain the barcode text
+`type`: optional, barcode type. Defaults to code39. Supported types are part of [GD::Barcode](https://metacpan.org/pod/GD::Barcode)
+`key`: optional, api key value
